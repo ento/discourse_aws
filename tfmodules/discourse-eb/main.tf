d1c41269b17795602012aca2e147a4a3947d90b0
Marica Odagaki <ento.entotto@gmail.com>
2019-08-09 19:37:29 -0800

    Enable default encryption on S3 buckets
    Sorry if this breaks anything for anyone using these modules

--- tfmodules/discourse-eb/main.tf
+++ tfmodules/discourse-eb/main.tf
@@ -5,6 +5,14 @@ data "aws_region" "current" {}
 resource "aws_s3_bucket" "files" {
   bucket = "discourse-files-${var.cname_prefix}-${data.aws_caller_identity.current.account_id}"
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   tags = "${var.tags}"
 
   # ignore lifecycle rule set up by discourse
--- tfmodules/discourse-mail-receiver/main.tf
+++ tfmodules/discourse-mail-receiver/main.tf
@@ -9,6 +9,14 @@ data "aws_region" "current" {}
 resource "aws_s3_bucket" "mailbox" {
   bucket = "discourse-mailbox-${var.cname_prefix}-${data.aws_caller_identity.current.account_id}"
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   tags = "${var.tags}"
 }
 
